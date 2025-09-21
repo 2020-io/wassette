72263eefa91d79b3d032ac1b25f1fc69ff11c57b
2020.io <github@2020.io>
2025-09-20 18:15:49 -0700

    How "fastmcp inspect" and "fastmcp run" are workig properly

--- install_fastmcp.sh
+++ install_fastmcp.sh
@@ -1,31 +1,69 @@
 #!/usr/bin/env bash
 
-echo "-> uv run --python=3.13 python --version" > /dev/stderr
-uv run --python=3.13 python --version
+export UV_NO_BUILD_ISOLATION=1
+
+#sudo uv pip uninstall --python=3.13 --system fastmcp
+#sudo uv pip uninstall --python=3.13 --system httpxx
+#sudo uv pip uninstall --python=3.13 --system asyncio
+#sudo uv pip uninstall --python=3.13 --system pandas
+#uv pip uninstall --python=3.13 fastmcp
+#uv pip uninstall --python=3.13 httpxx
+#uv pip uninstall --python=3.13 asyncio
+#uv pip uninstall --python=3.13 pandas
+
+ROOT=`dirname $0`
+pushd $ROOT >/dev/null
+export ROOT=`pwd`
+popd >/dev/null
+
+echo "Project root: $ROOT"
+ls -l "${ROOT}/requirements.txt"
+
+if [ ! -f "${ROOT}/requirements.txt" ]
+then
+  echo "Unable to locate requirements.txt"
+  echo "Re-run \"$0\" from the project root"
+  exit 1
+fi
+
+echo "-> pip install -r requirements.txt"
+#pip install -r "${ROOT}/requirements.txt"
 err=$?
-echo -e "<- uv run python --version returned $err\n" > /dev/stderr
+echo -e "<- pip install -r \"requirements.txt\" returned $err\n"
+if [ $err -ne 0 ]; then exit $err; fi
 
-echo "-> uv pip install fastmcp" > /dev/stderr
-#pip install fastmcp
-sudo uv pip install --python=3.13 --system fastmcp
-echo -e "<- uv pip install fastmcp returned $err\n" > /dev/stderr
-
-echo "-> uv pip install httpx" > /dev/stderr
-#pip install httpx
-sudo uv pip install --python=3.13 --system httpx
-err=$?
-echo -e "<- uv pip install httpx returned $err\n" > /dev/stderr
-
-echo "-> uv pip install pandas" > /dev/stderr
-#pip install pandas
-sudo uv pip install --python=3.13 --system pandas
-err=$?
-echo -e "<- uv pip install pandas returned $err\n" > /dev/stderr
-
-#echo "-> uv pip install asyncio" > /dev/stderr
-#pip install asyncio
-#sudo uv pip install --python=3.13 --system asyncio
+which python
+python --version
+#echo "-> uv run --python=3.13 python --version" > /dev/stderr
+#uv run --python=3.13 python --version
 #err=$?
-#echo -e "<- uv pip install asyncio returned $err\n" > /dev/stderr
+#echo -e "<- uv run python --version returned $err\n" > /dev/stderr
+
+echo "-> install fastmcp" > /dev/stderr
+#pip install --no-build-isolation fastmcp
+uv pip install --python=3.13 fastmcp
+err=$?
+echo -e "<- install fastmcp returned $err\n" > /dev/stderr
+
+echo "-> install httpx" > /dev/stderr
+#pip install --no-build-isolation httpx
+uv pip install --no-build-isolation --python=3.13 httpx
+err=$?
+echo -e "<- install httpx returned $err\n" > /dev/stderr
+
+#echo "-> install asyncio" > /dev/stderr
+#pip install --no-build-isolation asyncio
+#uv pip install --no-build-isolation --python=3.13 asyncio
+#err=$?
+#echo -e "<- install asyncio returned $err\n" > /dev/stderr
+
+echo "-> install pandas" > /dev/stderr
+#pip install --no-build-isolation pandas
+uv pip install --no-build-isolation --python=3.13 pandas
+err=$?
+echo -e "<- install pandas returned $err\n" > /dev/stderr
 
 uv pip list --python=3.13
+err=$?
+
+exit $err
