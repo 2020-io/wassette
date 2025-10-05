f9dad350f49cb283b70d5369e1520838a601fbc2
Matt Conover <mconover@gmail.com>
2025-10-05 03:11:07 -0700

    Remove 3.3 and change ../node_modules/@ruby to ../@ruby

--- assets/ruby/src/index.ts
+++ assets/ruby/src/index.ts
@@ -3,9 +3,8 @@ import { IRB } from "./irb-worker";
 import { makeJQueryTerminal } from "./terminals/jquery-terminal"
 import { makeXTermTerminal } from "./terminals/xterm";
 import { makeXtermPtyTerminal } from "./terminals/xterm-pty";
-import irb_3_3_wasm from "../node_modules/@ruby/3.3-wasm-wasi/dist/ruby.debug+stdlib.wasm?url";
-import irb_3_4_wasm from "../node_modules/@ruby/3.4-wasm-wasi/dist/ruby.debug+stdlib.wasm?url";
-import irb_head_wasm from "../node_modules/@ruby/head-wasm-wasi/dist/ruby.debug+stdlib.wasm?url"
+import irb_3_4_wasm from "../@ruby/3.4-wasm-wasi@2.7.1/dist/ruby.debug+stdlib.wasm?url";
+import irb_head_wasm from "../@ruby/head-wasm-wasi/dist/ruby.debug+stdlib.wasm?url"
 
 function makeTerminal(rubyVersion: string) {
     const query = new URLSearchParams(window.location.search);
@@ -26,8 +25,8 @@ function makeTerminal(rubyVersion: string) {
     return terminals[defaultTerminal]();
 }
 
-const rubyVersions = { "3.3": irb_3_3_wasm, "3.4": irb_3_4_wasm, "head": irb_head_wasm };
-const defaultRubyVersion = "head";
+const rubyVersions = { "3.4": irb_3_4_wasm, "head": irb_head_wasm };
+const defaultRubyVersion = "3.4";
 
 function deriveCurrentRubyVersion() {
     const query = new URLSearchParams(window.location.search);
